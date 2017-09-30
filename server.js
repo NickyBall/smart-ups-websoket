089f40ff8a752a06ed7d7f50d189f17d73ab538e
NickyBall <ball.vong.yo.gh@gmail.com>
2017-09-30 19:47:58 +0700

    broadcast v0.2

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-// wss.on('connection', (ws) => {
-//   console.log('Client connected');
-//   ws.on('close', () => console.log('Client disconnected'));
-// });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
 //
 // setInterval(() => {
 //   wss.clients.forEach((client) => {
@@ -33,13 +41,13 @@ const wss = new SocketServer({ server });
 //   });
 // };
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
-});
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     // Broadcast to everyone else.
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(data);
+//       }
+//     });
+//   });
+// });
