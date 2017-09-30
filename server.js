23259ad4cec3159f7a402c923a657b25f2402de6
NickyBall <ball.vong.yo.gh@gmail.com>
2017-09-30 19:43:48 +0700

    broadcast V0.1

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ const wss = new SocketServer({ server });
 // }, 1000);
 
 // Broadcast to all.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
+// wss.broadcast = function broadcast(data) {
+//   wss.clients.forEach(function each(client) {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(data);
+//     }
+//   });
+// };
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
